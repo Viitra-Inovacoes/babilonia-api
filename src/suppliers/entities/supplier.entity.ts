de9c94daa9d59e330589c54e46397ce4fb33b888
BrenoGMLima <breno.lima@viitra.com>
2026-01-27 09:25:32 -0300

    feat: altera colunas da tabela de forncedores

--- src/suppliers/entities/supplier.entity.ts
+++ src/suppliers/entities/supplier.entity.ts
@@ -1,20 +1,10 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  JoinColumn,
-  ManyToOne,
-} from 'typeorm';
-import { State } from '../../common/state.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Bank } from 'src/banks/entities/bank.entity';
 
-@Entity({ name: 'suppliers' })
+@Entity()
 export class Supplier {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-
-  @Column({ name: 'code', type: 'varchar', length: 50, unique: true })
-  code: string;
 
   @Column({ name: 'trade_name', type: 'varchar', length: 255 })
   tradeName: string;
@@ -22,8 +12,8 @@ export class Supplier {
   @Column({ name: 'corporate_name', type: 'varchar', length: 255 })
   corporateName: string;
 
-  @Column({ name: 'cnpj', type: 'varchar', length: 20, nullable: true })
-  cnpj?: string;
+  @Column({ name: 'cnpj', type: 'varchar', length: 20 })
+  cnpj: string;
 
   @Column({ name: 'cpf', type: 'varchar', length: 15, nullable: true })
   cpf?: string;
@@ -35,31 +25,29 @@ export class Supplier {
     name: 'state_registration',
     type: 'varchar',
     length: 50,
-    nullable: true,
   })
-  stateRegistration?: string;
+  stateRegistration: string;
 
-  @Column({ name: 'address', type: 'varchar', length: 255, nullable: true })
-  address?: string;
+  @Column({ name: 'address', type: 'varchar', length: 255 })
+  address: string;
 
-  @Column({ name: 'complement', type: 'varchar', length: 255, nullable: true })
-  complement?: string;
+  @Column({ name: 'complement', type: 'varchar', length: 255 })
+  complement: string;
 
-  @Column({ name: 'city', type: 'varchar', length: 150, nullable: true })
-  city?: string;
+  @Column({ name: 'city', type: 'varchar', length: 150 })
+  city: string;
 
-  @ManyToOne(() => State, { nullable: true })
-  @JoinColumn({ name: 'state_id' })
-  state?: State;
+  @Column({ name: 'state', type: 'varchar', length: 150 })
+  state: string;
 
-  @Column({ name: 'postal_code', type: 'varchar', length: 20, nullable: true })
-  postalCode?: string;
+  @Column({ name: 'postal_code', type: 'varchar', length: 20 })
+  postalCode: string;
 
-  @Column({ name: 'country', type: 'varchar', length: 100, nullable: true })
-  country?: string;
+  @Column({ name: 'country', type: 'varchar', length: 100 })
+  country: string;
 
-  @Column({ name: 'contact', type: 'varchar', length: 255, nullable: true })
-  contact?: string;
+  @Column({ name: 'contact', type: 'varchar', length: 255 })
+  contact: string;
 
   @Column({
     name: 'commercial_contact',
@@ -80,10 +68,10 @@ export class Supplier {
   @Column({ name: 'notes', type: 'text', nullable: true })
   notes?: string;
 
-  @ManyToOne(() => Bank, (bank) => bank.suppliers)
-  @JoinColumn({ name: 'bank_id' })
-  bank: Bank;
-
+  @ManyToOne(() => Bank, (bank) => bank.suppliers, {
+    nullable: true,
+    eager: true,
+  })
   @Column({
     name: 'checking_account',
     type: 'varchar',
