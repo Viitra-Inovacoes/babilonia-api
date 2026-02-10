f8f22db9483eca195b1895fcb14b3e75929a519a
BrenoGMLima <breno.lima@viitra.com>
2026-02-10 09:22:07 -0300

    docs: confira a documentação da api com swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Babilônia API')
+    .setVersion('1.0')
+    .addTag('babilonia')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,18 @@
 import { IsString, IsNotEmpty, MinLength, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'João Silva',
+    description: 'Nome completo do usuário',
+  })
   nome: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'joaosilva', description: 'Login do usuário' })
   login: string;
 
   @IsString()
@@ -16,5 +22,6 @@ export class CreateUserDto {
     message:
       'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
   })
+  @ApiProperty({ example: 'P@ssw0rd123', description: 'Senha do usuário' })
   password: string;
 }
